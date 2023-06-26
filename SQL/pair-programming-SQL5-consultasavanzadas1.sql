0693db2b5847eec87860e951254154d76737004b
CrisSangrador <cris.sangrador@gmail.com>
2023-06-26 12:58:19 +0200

    pair sql hecho

---
+++ SQL/pair-programming-SQL5-consultasavanzadas1.sql
@@ -0,0 +1,28 @@
+SELECT MIN(unit_price) AS lowestPrice, 
+MAX(unit_price) AS highestPrice
+FROM products;
+
+SELECT COUNT(units_in_stock),
+AVG(unit_price)
+FROM products;
+-- Dudábamos entre units_in_stock o product_id, así que nos hemos decidido por units_in_stock.
+
+SELECT MAX(freight),
+MIN(freight)
+FROM orders
+WHERE ship_country = "UK";
+
+SELECT AVG(unit_price)
+FROM products;
+
+SELECT product_name, unit_price
+FROM products
+WHERE unit_price > 28.86
+ORDER BY unit_price DESC;
+
+SELECT COUNT(discontinued)
+FROM products
+WHERE discontinued = 1;
+
+
+
